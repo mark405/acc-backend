d3f66cd24a7813d9d439d14985d23124fa542c28
MarkRufin-Zavhorodni <Mark.Rufin-Zavhorodnii@nangasystems.com>
2026-04-08 23:10:11 +0300

    f

--- src/main/java/com/traffgun/acc/controller/OfferController.java
+++ src/main/java/com/traffgun/acc/controller/OfferController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -49,6 +50,7 @@ public class OfferController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         offerService.delete(id);
     }
